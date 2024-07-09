89d4718cfdfd734024d18d8655c7ff49052f4121
YounesJabbour <younes0jabbour@gmail.com>
2024-07-09 10:20:57 +0100

    vf

--- routes/client.js
+++ routes/client.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 const clientController = require("../controller/clientController");
 
-
 router.get("/", clientController.listClient);
 router.get("/:id", clientController.getClient);
 router.post("/create", clientController.createClient);
