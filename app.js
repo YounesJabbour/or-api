91143f760c3ff63dc89fcb390f46a38bfd483c16
YounesJabbour <younes0jabbour@gmail.com>
2024-07-09 09:46:31 +0100

    changing the structure of the project

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 require("dotenv").config();
-var createError = require("http-errors");
 var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
 
 var ClientRouter = require("./routes/client");
 var SiteRouter = require("./routes/site");
@@ -13,10 +9,8 @@ var indexRouter = require("./routes/index");
 
 var app = express();
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use("/client", ClientRouter);
 app.use("/vehicule", vehiculeRouter);
@@ -24,20 +18,7 @@ app.use("/site", SiteRouter);
 app.use("/ordre", orderRouter);
 app.use("/", indexRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.listen(process.env.PORT || 3000, () => {
+  console.log("`OR-authall` is running on port ", process.env.PORT || 3000);
 });
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-module.exports = app;
+``;
